dd939ca8ed91683c78146bbcae91e51f65f4dab7
rohanmanatkar <rohanmanatkar@gmail.com>
2016-04-22 01:29:19 +0530

    Added files via upload

---
+++ make1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int a, b;
+	scanf("%d%d\n", &a, &b);
+	printf("%d", x(a, b));
+	printf("%d", g());
+	return 0;
+}
---
+++ make2.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int x(int x, int y)
+{
+	return x + y;
+}
