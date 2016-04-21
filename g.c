d6cb0e2b5b5de72262a26f32375244cee1c9248b
rohanmanatkar <rohanmanatkar@gmail.com>
2016-04-22 02:13:27 +0530

    modified by rohan

--- g.c
+++ g.c
@@ -1,13 +1,6 @@
 int g() {
-asdf
-    asasdfkasjdf;lakjsdf;lkjasd;flkj
-    asdasdfaf
-    asalkjfhsdfsdfakjfsdh'asfd
-    as
-    fda
-    sfd
-    as
-    fd
 }
-
+hi this is rohan
 Hello This is modified by me Vaibhav
+asjdaksjda
+sdas
