9af495d829c599f5f1796908dd29862ecd4f9ecf
Abhilash Mhaisne <55abhilash@openmailbox.org>
2016-04-22 02:33:08 +0530

    Modified make2.c

--- make2.c
+++ make2.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 int x(int x, int y)
 {
-	asdasdsd;
+	asdasdsadflasjdflkasjdf
+        alsjdkf;las
+        as
+        df
+        asdf
+        as
+        df
+        sd;
 	bullshit;
 	return x ;
 }
